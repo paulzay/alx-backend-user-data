ea029a2f01459b311bd6c3a4731b22a61e062c0c
Paul Ogolla <paulotieno2@gmail.com>
2024-09-11 17:44:18 +0300

    encrypt and check

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" encrypting passwords"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ return salted password """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
