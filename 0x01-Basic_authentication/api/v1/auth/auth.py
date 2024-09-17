15dc9c3eed7d3cd6cecbfae23006356996222808
Paul Ogolla <paulotieno2@gmail.com>
2024-09-17 23:12:00 +0300

    update methods

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,11 +5,15 @@ from flask import request
 
 
 class Auth:
+    """ class definition"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require suth method """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """ auth hesder method"""
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """ current user method """
         return None
