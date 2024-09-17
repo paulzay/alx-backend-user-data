50986120d3eca8e31c2c298ef21a839ebf9c8417
Paul Ogolla <paulotieno2@gmail.com>
2024-09-17 23:37:05 +0300

    rollback

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,9 +10,8 @@ class Auth:
         """ require suth method """
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
-        for paths in excluded_paths:
-            if paths.endswith('*') and path.startswith(paths[:-1]):
-                return False
+        if path in excluded_paths:
+            return False
         return True
 
     def authorization_header(self, request=None) -> str:
