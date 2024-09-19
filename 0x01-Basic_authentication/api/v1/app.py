236a1cdba4e7ea2f6b814a438e57e9e65c3270f4
Paul Ogolla <paulotieno2@gmail.com>
2024-09-20 00:09:07 +0300

    task  5

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -48,16 +48,20 @@ def forbidden(error) -> str:
 @app.before_request
 def before_request():
     """ request filters """
-    paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
+              '/api/v1/auth_session/login/']
     if auth is None:
         return
     if not auth.require_auth(request.path, paths):
         return
-    if not auth.authorization_header(request):
+    if not auth.authorization_header(request) and not auth.session_cookie(request):
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
     auth = auth.current_user(request)
+    if auth is None:
+        abort(401)
+    request.current_user = auth
 
 
 if __name__ == "__main__":
