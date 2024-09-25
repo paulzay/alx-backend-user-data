1917bfd4da1b6e47b143fb840d847af1100434fe
Paul Ogolla <paulotieno2@gmail.com>
2024-09-25 19:21:13 +0300

    auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,7 +2,22 @@
 """Auth module
 """
 import bcrypt
+from db import DB
 
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+    
+    def register_user(self, email: str, password: str):
+        user = self._db.find_user_by(email=email)
+        if user is not None:
+            raise ValueError('User emsil already exists')
+        else:
+          return self._db.add_user(email=email, hashed_password=password)
 
 def _hash_password(password: str) -> str:
     """Hash password
