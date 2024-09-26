c8b40cc282e0fd4835d3b2c0d8c4a348d09d53b0
Paul Ogolla <paulotieno2@gmail.com>
2024-09-26 21:49:20 +0300

    uodate auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -14,6 +14,7 @@ class Auth:
     """Auth class to interact with the authentication database.
     """
     def __init__(self):
+        """ initialize db"""
         self._db = DB()
     
     def register_user(self, email: str, password: str):
@@ -74,9 +75,3 @@ class Auth:
             raise ValueError
         self._db.update_user(user.id, hashed_password=new_password, reset_token=None)
         return None
-    
-    def __repr__(self):
-        return 'Auth()'
-    
-    def __str__(self):
-        return 'Auth()'
