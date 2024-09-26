7c674cac6c675cd488ce355f5a04d5135353f3b9
Paul Ogolla <paulotieno2@gmail.com>
2024-09-26 22:17:10 +0300

    fix issues

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -17,7 +17,7 @@ class Auth:
     def __init__(self):
         """ initialize db"""
         self._db = DB()
-    
+
     def register_user(self, email: str, password: str):
         """Register a user
         """
@@ -25,8 +25,8 @@ class Auth:
         if user is not None:
             raise ValueError('User emsil already exists')
         else:
-          return self._db.add_user(email=email, hashed_password=password)
-    
+            return self._db.add_user(email=email, hashed_password=password)
+
     def valid_login(self, email: str, password: str) -> bool:
         """Validate login
         """
@@ -34,7 +34,7 @@ class Auth:
         if user is None:
             return False
         return bcrypt.checkpw(password.encode(), user.hashed_password)
-    
+
     def create_session(self, email: str) -> str:
         """Create a session
         """
@@ -42,7 +42,7 @@ class Auth:
         session_id = bcrypt.gensalt()
         self._db.update_user(user.id, session_id=session_id)
         return session_id
-    
+
     def get_user_from_session_id(self, session_id: str) -> str:
         """Get user from session id
         """
@@ -52,11 +52,12 @@ class Auth:
         if user is None:
             return
         return user.email
-    
+
     def destroy_session(self, user_id: int):
         """Destroy a session
         """
         self._db.update_user(user_id, session_id=None)
+        return None
 
     def get_reset_password_token(self, email: str) -> str:
         """Get reset password token
@@ -65,7 +66,7 @@ class Auth:
         token = bcrypt
         self._db.update_user(user.id, reset_token=token)
         return token
-    
+
     def update_password(self, email: str, reset_token: str, new_password: str):
         """Update password
         """
@@ -74,5 +75,6 @@ class Auth:
             raise ValueError
         if not bcrypt.checkpw(reset_token.encode(), user.reset_token):
             raise ValueError
-        self._db.update_user(user.id, hashed_password=new_password, reset_token=None)
+        self._db.update_user(user.id, hashed_password=new_password,
+                             reset_token=None)
         return None
