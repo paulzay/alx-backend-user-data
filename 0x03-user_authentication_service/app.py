31c745e0cef32e270fa2dda48b56bdf9c8d5b474
Paul Ogolla <paulotieno2@gmail.com>
2024-09-26 22:03:46 +0300

    sessions and auth

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """ flask app """
 from flask import Flask, jsonify
+from auth import Auth
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
@@ -14,5 +17,41 @@ def index() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', methods=['GET'], strict_slashes=False)
+def users(email: str, password: str) -> str:
+    """ GET /users
+    Return:
+      - the status of the API
+    """
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "User created"})
+    except ValueError:
+        return jsonify({"message": "User already exists"})
+
+
+@app.route('/sessions', methods=['GET'], strict_slashes=False)
+def sessions(email: str, password: str) -> str:
+    """ GET /sessions
+    Return:
+      - the status of the API
+    """
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        return jsonify({"email": email, "message": "Session created"})
+    else:
+        return jsonify({"message": "Wrong password"})
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def sessions_delete(user_id: int) -> str:
+    """ DELETE /sessions
+    Return:
+      - the status of the API
+    """
+    AUTH.destroy_session(user_id)
+    return jsonify({"message": "Session deleted"})
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5001")
+    app.run(host="0.0.0.0", port="5000")
